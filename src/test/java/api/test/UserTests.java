3f6cba4aff5f8e174c6c74ff24d976f5d457e36c
Sanket <sanket.butle@bonamisoftware.com>
2026-05-11 21:56:56 +0530

    pushing some changes

--- src/test/java/api/test/UserTests.java
+++ src/test/java/api/test/UserTests.java
@@ -45,7 +45,7 @@ public class UserTests {
 		
 		Assert.assertEquals(response.getStatusCode(), 200);
 		System.out.println("Username: " + this.userPayload.getUsername());
-		System.out.println("Webhook Trigger Testt");
+		System.out.println("Webhook Trigger Test");
 		
 		logger.info("*******User is Created********");
 	}
